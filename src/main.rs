9de53bfbb79fc908005d6ba830339cae7289f0b6
okrplay <32576280+okrplay@users.noreply.github.com>
2018-10-06 12:52:10 +0200

    Added dependencies

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+//
+//  Requirements for building: https://gtk-rs.org/docs-src/requirements.html
+//  Dependencies needed in Cargo.toml: gtk = "0.5.0"
+//
+
+extern crate gtk;
+
+use gtk::prelude::*;
+
+fn main() {
+    println!("Hello, world!");
+}
